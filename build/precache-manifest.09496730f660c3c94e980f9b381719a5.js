b08132e38791195f399b326892ab62c202b5b42a
eunbin-lee <66936228+eunbin-lee@users.noreply.github.com>
2020-08-13 23:55:02 +0900

    Making one page 'Movie App' from Nomad Coders
    Complete this course.
    
    The original folders have lots of files 
    so I uploaded this without node_modules folder.

---
+++ build/precache-manifest.09496730f660c3c94e980f9b381719a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a71fddd57a1eb8f992004cdec4de78f4",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "3920fb683acf523452b8",
+    "url": "/movie_app/static/css/main.ed64d2b0.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3920fb683acf523452b8",
+    "url": "/movie_app/static/js/main.4b8dde4f.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
